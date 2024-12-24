f1bff8ec561d7a45eb632196046bba981b858f35
Trent Mitchell-Borley <trent@trents.computer>
2024-12-25 09:55:58 +1300

    lazy fix til i can do the real fix

--- src/pages/home-grid/FooterTextItem.tsx
+++ src/pages/home-grid/FooterTextItem.tsx
@@ -1,10 +1,9 @@
-import { FooterText } from "../FooterText";
 import { GridItem } from "./components";
 
 export function FooterTextItem(props: Record<string, unknown>) {
   return (
     <GridItem {...props} css={{ px: 16, py: 0, justifyContent: "flex-start" }}>
-      <FooterText />
+      {/* <FooterText /> */}
     </GridItem>
   );
 }
