a5c90014ebd6b8245e46dd8d4e61f2d3dc910f4e
Trent Mitchell-Borley <trent@trents.computer>
2025-02-05 22:28:44 +1300

    and again

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,13 +3,13 @@ import { createMiddleware } from "@solidjs/start/middleware";
 export default createMiddleware({
   onRequest: [
     async (event) => {
-      event.nativeEvent.headers.set("Access-Control-Allow-Origin", "*");
-      event.nativeEvent.headers.set(
+      event.response.headers.set("Access-Control-Allow-Origin", "*");
+      event.response.headers.set(
         "Access-Control-Allow-Methods",
         "GET,HEAD,PUT,PATCH,POST,DELETE"
       );
-      event.nativeEvent.headers.set("Access-Control-Allow-Headers", "*");
-      event.nativeEvent.headers.set("Access-Control-Expose-Headers", "*");
+      event.response.headers.set("Access-Control-Allow-Headers", "*");
+      event.response.headers.set("Access-Control-Expose-Headers", "*");
 
       if (event.nativeEvent.method.toUpperCase() === "OPTIONS") {
         const res = new Response(null, { status: 204 });
