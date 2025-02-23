caf782752885467740b4a4f004a7dfff7272b058
Trent Mitchell-Borley <trent@trents.computer>
2025-02-23 21:53:59 +1300

    Lil tweaks

--- src/app.tsx
+++ src/app.tsx
@@ -1,38 +1,20 @@
 // @refresh reload
 import { QueryClient, QueryClientProvider } from "@tanstack/solid-query";
-import "./styles/app.css";
-import "./styles/reset.css";
-import "./styles/soehne-mono.css";
-import "./styles/soehne.css";
-
-// export default function App() {
-//   return (
-//     <Router
-//       root={(props) => (
-//         <QueryClientProvider client={new QueryClient()}>
-//           <MetaProvider>
-//             <Title>Trents PC</Title>
-//             <Suspense>{props.children}</Suspense>
-//           </MetaProvider>
-//         </QueryClientProvider>
-//       )}
-//     >
-//       <FileRoutes />
-//     </Router>
-//   );
-// }
-
 import { RouterProvider, createRouter } from "@tanstack/solid-router";
 import { routeTree } from "./routeTree.gen";
+
+import "./styles/reset.css";
+
+import "./styles/app.css";
+import "./styles/soehne-mono.css";
+import "./styles/soehne.css";
 
 const router = createRouter({
   routeTree,
   defaultPreload: "intent",
-  defaultStaleTime: 5000,
   scrollRestoration: true,
 });
 
-// Register things for typesafety
 declare module "@tanstack/solid-router" {
   interface Register {
     router: typeof router;
