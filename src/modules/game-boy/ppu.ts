26b4401ea264d0939adf8f389d742a9ff299b09e
Trent Mitchell-Borley <trent@trents.computer>
2025-04-07 23:05:49 +1200

    scanline renderer

--- src/modules/game-boy/ppu.ts
+++ src/modules/game-boy/ppu.ts
@@ -19,6 +19,15 @@ export function createPPU(
 
   function step() {
     lcdX++;
+    if (lcdX === 252 && lcdY % 8 === 0 && lcdY < 144) {
+      // Render the scanline
+
+      if (lcdY === 0) {
+        // Clear the screen
+        clearFrame();
+      }
+      drawScanline();
+    }
     if (lcdX > 455) {
       lcdX = 0;
       lcdY++;
@@ -45,7 +54,8 @@ export function createPPU(
         );
       }
       if (lcdY === 144) {
-        renderScreen(gameBoy, getCanvas());
+        // renderScreen(gameBoy, getCanvas());
+        commitFrame();
         gameBoy.addressBus.writeByte(
           0xff0f,
           gameBoy.addressBus.readByte(0xff0f) | 0x01
@@ -71,7 +81,9 @@ export function createPPU(
    * get cached tile data from starting address of tile
    */
   const tileDataCache = new Map<number, OffscreenCanvas>();
-  const transparentTileDataCache = new Map<number, OffscreenCanvas>();
+
+  const tileMap = new OffscreenCanvas(256, 256);
+  const quadTileMap = new OffscreenCanvas(512, 512);
 
   function invalidateTileDataCache(address: number) {
     const startingAddress = address & 0xfff0;
@@ -93,10 +105,10 @@ export function createPPU(
         const highBit = (highByte >> (7 - x)) & 1;
         const color = lowBit + highBit * 2;
 
-        if (transparent && color === 0) {
-          continue;
-        }
-        ctx.fillStyle = gameBoy.colors[color];
+        // if (color === 0) {
+        //   continue;
+        // }
+        ctx.fillStyle = color === 0 ? "transparent" : gameBoy.colors[color];
 
         ctx.fillRect(x, y, 1, 1);
       }
@@ -113,22 +125,14 @@ export function createPPU(
     return tileCanvas;
   }
 
-  function getObjectTile(startingAddress: number) {
-    if (transparentTileDataCache.has(startingAddress)) {
-      return transparentTileDataCache.get(startingAddress)!;
-    }
-    const tileCanvas = renderTile(gameBoy, startingAddress, true);
-    transparentTileDataCache.set(startingAddress, tileCanvas);
-    return tileCanvas;
-  }
-
   function renderTileMap(gameBoy: GameBoy) {
-    const canvas = new OffscreenCanvas(256, 256);
+    const canvas = tileMap;
     const ctx = canvas.getContext("2d")!;
 
     const lcdControl = gameBoy.addressBus.readByte(0xff40);
     const addressingMode = (lcdControl & 0x10) >> 4;
 
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     for (let y = 0; y < 32; y++) {
       for (let x = 0; x < 32; x++) {
         const address = 0x9800 + y * 32 + x;
@@ -147,8 +151,9 @@ export function createPPU(
   }
 
   function renderQuadTileMap(gameBoy: GameBoy) {
-    const canvas = new OffscreenCanvas(256 * 4, 256 * 4);
+    const canvas = quadTileMap;
     const ctx = canvas.getContext("2d")!;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     const tileMap = renderTileMap(gameBoy);
     ctx.drawImage(tileMap, 0, 0);
     ctx.drawImage(tileMap, 256, 0);
@@ -158,8 +163,8 @@ export function createPPU(
   }
 
   function drawObject(gameBoy: GameBoy, address: number) {
-    const canvas = new OffscreenCanvas(160, 144);
-    const ctx = canvas.getContext("2d")!;
+    // const canvas = new OffscreenCanvas(160, 144);
+    // const ctx = canvas.getContext("2d")!;
     const y = gameBoy.addressBus.readByte(address);
     const x = gameBoy.addressBus.readByte(address + 1);
     const tileNumber = gameBoy.addressBus.readByte(address + 2);
@@ -169,21 +174,41 @@ export function createPPU(
     const flipX = (attributes & 0x20) !== 0;
     const flipY = (attributes & 0x40) !== 0;
 
-    const sourceTile = renderTile(gameBoy, tileAddress, true);
-    const tileCanvas = new OffscreenCanvas(8, 8);
-    const tileCtx = tileCanvas.getContext("2d")!;
-    if (flipX) {
-      tileCtx.scale(-1, 1);
-      tileCtx.translate(0 - tileCanvas.width, 0);
-    }
-    if (flipY) {
-      tileCtx.scale(1, -1);
-      tileCtx.translate(0, 0 - tileCanvas.height);
-    }
+    const sourceTile = getTile(tileAddress);
+    // const tileCanvas = new OffscreenCanvas(8, 8);
+    // const tileCtx = tileCanvas.getContext("2d")!;
+    // if (flipX) {
+    //   tileCtx.scale(-1, 1);
+    //   tileCtx.translate(0 - tileCanvas.width, 0);
+    // }
+    // if (flipY) {
+    //   tileCtx.scale(1, -1);
+    //   tileCtx.translate(0, 0 - tileCanvas.height);
+    // }
 
-    tileCtx.drawImage(sourceTile, 0, 0);
+    // tileCtx.drawImage(sourceTile, 0, 0);
 
-    ctx.drawImage(tileCanvas, x - 8, y - 16);
+    // ctx.drawImage(tileCanvas, x - 8, y - 16);
+    return {
+      tile: sourceTile,
+      x: x - 8,
+      y: y - 16,
+    };
+  }
+
+  let tempScreen = new OffscreenCanvas(160, 144);
+  function renderScreenOffscreen(gameBoy: GameBoy) {
+    const canvas = tempScreen;
+    const ctx = canvas.getContext("2d")!;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    const tileMap = renderQuadTileMap(gameBoy);
+    const SCY = gameBoy.addressBus.readByte(0xff42);
+    const SCX = gameBoy.addressBus.readByte(0xff43);
+    ctx.drawImage(tileMap, -SCX, -SCY);
+    for (let addr = 0xfe00; addr < 0xfea0; addr += 4) {
+      const { tile, x, y } = drawObject(gameBoy, addr);
+      ctx.drawImage(tile, x, y);
+    }
     return canvas;
   }
 
@@ -198,9 +223,33 @@ export function createPPU(
     ctx.drawImage(tileMap, -SCX, -SCY);
 
     for (let addr = 0xfe00; addr < 0xfea0; addr += 4) {
-      const canvas = drawObject(gameBoy, addr);
-      ctx.drawImage(canvas, 0, 0);
+      const { tile, x, y } = drawObject(gameBoy, addr);
+      ctx.drawImage(tile, x, y);
     }
+  }
+
+  const inProgressCanvas = new OffscreenCanvas(160, 144);
+  const inProgressCtx = inProgressCanvas.getContext("2d")!;
+
+  function clearFrame() {
+    inProgressCtx.clearRect(
+      0,
+      0,
+      inProgressCtx.canvas.width,
+      inProgressCtx.canvas.height
+    );
+  }
+
+  function drawScanline() {
+    const line = lcdY;
+    const screen = renderScreenOffscreen(gameBoy);
+    inProgressCtx.drawImage(screen, 0, line, 160, 8, 0, line, 160, 8);
+  }
+
+  function commitFrame() {
+    const ctx = getCanvas().getContext("2d")!;
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.drawImage(inProgressCanvas, 0, 0);
   }
 
   return { step, getLCDX, getLCDY, invalidateTileDataCache };
