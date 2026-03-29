070c5e5e4b1626e6f9e7cc6c6dbdb57df5a773d8
trent <trent@trents.computer>
2026-03-29 22:20:51 +1300

    fix slider alignment

--- src/modules/pixel-font-editor/Slider.tsx
+++ src/modules/pixel-font-editor/Slider.tsx
@@ -1,3 +1,5 @@
+import { styled } from "@hypergood/css";
+
 const GAMEBOY = ["#a3b334", "#6B882E", "#3A6122", "#0F3810"];
 
 export type SliderProps = {
@@ -28,16 +30,12 @@ export function Slider(props: SliderProps) {
   }
 
   return (
-    <div
-      style={{
-        height: "8px",
-        width: "100px",
-        position: "relative",
-      }}
+    <Root
       ref={ref}
       onPointerDown={(event) => {
         const target = event.target as HTMLElement;
         target.setPointerCapture(event.pointerId);
+        handleSliderMove(event);
       }}
       onPointerMove={(event) => {
         const target = event.target as HTMLElement;
@@ -50,12 +48,43 @@ export function Slider(props: SliderProps) {
         }
       }}
     >
-      <div
+      <Track
         style={{
           background: GAMEBOY[1],
-          height: "100%",
-          width: "100%",
-          "clip-path": `polygon(
+        }}
+      >
+        <Indicator
+          style={{
+            width: percentage() * 100 + "%",
+            height: "100%",
+            background: GAMEBOY[3],
+          }}
+        />
+      </Track>
+      <Thumb
+        style={{
+          position: "absolute",
+          top: "-4px",
+          left: `calc(${percentage() * 100}% - ${2 * percentage()}px)`,
+          height: "16px",
+          width: "2px",
+          background: GAMEBOY[3],
+        }}
+      />
+    </Root>
+  );
+}
+
+const Root = styled("div", {
+  height: "8px",
+  width: "100px",
+  position: "relative",
+});
+
+const Track = styled("div", {
+  height: "100%",
+  width: "100%",
+  "clip-path": `polygon(
         0px calc(100% - 2px),
         2px calc(100% - 2px),
         2px 100%,
@@ -69,26 +98,7 @@ export function Slider(props: SliderProps) {
         2px 2px,
         0px 2px
         )`,
-        }}
-      >
-        <div
-          style={{
-            width: percentage() * 100 + "%",
-            height: "100%",
-            background: GAMEBOY[3],
-          }}
-        />
-      </div>
-      <div
-        style={{
-          position: "absolute",
-          top: "-4px",
-          left: percentage() * 100 + "%",
-          height: "16px",
-          width: "2px",
-          background: GAMEBOY[3],
-        }}
-      />
-    </div>
-  );
-}
+});
+
+const Indicator = styled("div", {});
+const Thumb = styled("div", {});
