d3fb667203f6d1f348939e370d22757d445e9f3a
Trent <trent@trents.computer>
2026-07-08 20:21:39 +1200

    remove old pantry/drizzle stuff

--- src/routes-legacy/api/test.ts
+++
@@ -1,8 +0,0 @@
-import type { APIEvent } from "@solidjs/start/server";
-import { json } from "~/server/json";
-
-export async function GET(event: APIEvent) {
-  return json({
-    hello: "world",
-  });
-}
